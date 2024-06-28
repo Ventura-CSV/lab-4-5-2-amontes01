429c0f587639d493453a1ce34bf135f5719d873d
Anthony Montes <anthony_montes4@my.vcccd.edu>
2024-06-27 18:01:45 -0700

    Lab 4-5

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    while total <= 100:
+        num = random.randint(1, 100)
+        numbers.append(num)
+        total += num
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
